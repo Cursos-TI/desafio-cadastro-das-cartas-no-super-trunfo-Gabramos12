c21b79097f112d19bd416d336c42e414896be904
Gabramos12 <112515416+Gabramos12@users.noreply.github.com>
2025-07-30 21:39:23 +0000

    modi

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,77 +7,78 @@
 //Teste larissa
 
 int main() {
-   char estado[20],Estado[20];
-   char codigo[20], Codigo[20];
-   char nomeCidade[50],b2nomeCidade[50];
+   char estado[20];
+   char codigo[20];
+   char nomeCidade[20];
    int Cartas ;
-   int populacao,b2populacao; 
+   int populacao,b2populacao;
    float Área,b2Área;
    float PIB,b2PIB;  
    int numeroturisticos,b2numeroturisticos;
-   
+   char Codigo[20];
+   char b2nomeCidade[20];
+   char Estado[20];
 
-
+//carta 01
    printf("Carta 01 \n");
 
    printf("Estado = "); 
    scanf("%c",&estado);
 
-  printf("Código = "); 
-   scanf("%c",&codigo);
-
-  printf("Nome da cidade = "); 
-   scanf("%d",&nomeCidade);
-
    printf("População = "); 
    scanf("%d",&populacao);
 
+   printf("Nome da cidade = "); 
+   scanf("%s",&nomeCidade);
+
    printf(" Área = ");
    scanf("%f", &Área);
+
+   printf("Código = "); 
+   scanf("%s",&codigo);
 
    printf(" PIB = ");
    scanf("%f", &PIB);
 
    printf("Número de pontos turistico = ");
    scanf("%d",&numeroturisticos);
-
+ //retorno de informações
    printf("Carta 01 \n");
-    printf("Estado: %c\n Código: %c\n",estado,codigo);
-    printf("Nome da Cidade: %c\n", nomeCidade);
-   printf("População: %d\n Área : %f\n",populacao, Área);
-   printf("PIB:%f\n  Número de Turístico: %d\n",PIB, numeroturisticos);
+   printf("Estado: %c\nPopulação: %d\n", estado , populacao);
+   printf("Nome da Cidade: %s\n", nomeCidade);
+   printf(" Código: %s\n Área : %f\n",codigo, Área);
+   printf("PIB:%f\n  Número de Turístico: %d\n", PIB , numeroturisticos );
    
+  //carta 02
 
     printf("Carta 02 \n");
 
-   printf("Estado = "); 
-   scanf("%c",&Estado);
+    printf("Estado = "); 
+    scanf("%s", &Estado);
 
-  printf("Código = "); 
-   scanf("%c",&Codigo);
-
-  printf("Nome da cidade = "); 
-   scanf("%d",&b2nomeCidade);
-
-
-    
     printf(" População = "); 
     scanf("%d",&b2populacao);
+    
+    printf("Nome da cidade = "); 
+    scanf("%s", &b2nomeCidade);
 
     printf(" Área = ");
     scanf("%f", &b2Área);
+    
+    printf("Código = "); 
+    scanf("%s", &Codigo);
 
     printf(" PIB = ");
     scanf("%f", &b2PIB);
 
     printf(" Número de pontos turistico = ");
     scanf("%d",&b2numeroturisticos);
-
+  //retorno de informações
     printf(" Carta 02 \n");
-    printf("Estado: %c\n Código: %c\n",Estado,Codigo);
-    printf("Nome da Cidade: %c\n", b2nomeCidade);
-    printf("População: %d\n Área : %f\n ",b2populacao, b2Área);
-    printf("Seu PIB: %f\n  Número de pontos Turístico: %d\n",b2PIB, b2numeroturisticos);
+    printf("Estado: %s\n População: %d\n", Estado , b2populacao);
+    printf("Nome da Cidade: %s\n", b2nomeCidade);
+    printf("Área : %f\n  Código: %s\n ",Codigo , b2Área);
+    printf("Seu PIB: %f\n  Número de pontos Turístico: %d\n", b2PIB , b2numeroturisticos);
 
  
 
